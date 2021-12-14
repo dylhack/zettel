3d7b9eff893dc4115e6f8bfdb0d3fad3c803b5e3
Dylan Hackworth <dylhack@gmail.com>
2021-12-14 14:35:36 -0600

    Added seconds

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,10 @@ use chrono::Local;
 fn get_now() -> (String, String) {
     // The formats are composed by following the strftime specifiers
     // https://docs.rs/chrono/latest/chrono/format/strftime/index.html#specifiers
-    // regular_format = "202112141345"
-    // spaced_format = "2021-12-14 1345"
-    let regular_format = "%Y%m%d%H%M";
-    let spaced_format = "%Y-%m-%d %H%M";
+    // regular_format = "20211214134505"
+    // spaced_format = "2021-12-14 134505"
+    let regular_format = "%Y%m%d%H%M%S";
+    let spaced_format = "%Y-%m-%d %H%M%S";
 
     let now = Local::now();
     let regular = now.format(regular_format).to_string();
